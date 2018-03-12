7efdee79e763ac74f1e57d453435e5cfafcae5ca
evopen <520dhh@gmail.com>
2018-03-13 00:03:46 +0800

    T09.04 Completed

--- Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -16,16 +16,25 @@
 
 package com.example.android.todolist;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.example.android.todolist.data.TaskContract;
 
 
 public class AddTaskActivity extends AppCompatActivity {
 
     // Declare a member variable to keep track of a task's selected mPriority
     private int mPriority;
+    private EditText mItemTitleEditText;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,8 @@ public class AddTaskActivity extends AppCompatActivity {
         // Initialize to highest mPriority by default (mPriority = 1)
         ((RadioButton) findViewById(R.id.radButton1)).setChecked(true);
         mPriority = 1;
+
+        mItemTitleEditText = (EditText) findViewById(R.id.editTextTaskDescription);
     }
 
 
@@ -43,13 +54,23 @@ public class AddTaskActivity extends AppCompatActivity {
      * It retrieves user input and inserts that new task data into the underlying database.
      */
     public void onClickAddTask(View view) {
-        // Not yet implemented
-        // TODO (6) Check if EditText is empty, if not retrieve input and store it in a ContentValues object
 
-        // TODO (7) Insert new task data via a ContentResolver
+        String input = mItemTitleEditText.getText().toString();
+        ContentValues cv = new ContentValues();
+        if (!input.isEmpty()) {
+            cv.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, input);
+            cv.put(TaskContract.TaskEntry.COLUMN_PRIORITY, mPriority);
 
-        // TODO (8) Display the URI that's returned with a Toast
+        }
+        // COMPLETED (6) Check if EditText is empty, if not retrieve input and store it in a ContentValues object
+
+        ContentResolver contentResolver = getContentResolver();
+        // COMPLETED (7) Insert new task data via a ContentResolver
+        Uri itemUri = contentResolver.insert(TaskContract.TaskEntry.CONTENT_URI, cv);
+        Toast.makeText(this, itemUri.toString(), Toast.LENGTH_SHORT).show();
+        // COMPLETED (8) Display the URI that's returned with a Toast
         // [Hint] Don't forget to call finish() to return to MainActivity after this insert is complete
+        finish();
 
     }
 
