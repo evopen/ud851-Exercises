7df3a92b52de0bb86a04a2170c061d7c11d8ca36
evopen <520dhh@gmail.com>
2018-03-15 14:35:23 +0800

    T09.05 Completed
    Signed-off-by: evopen <520dhh@gmail.com>

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -120,15 +120,34 @@ public class TaskContentProvider extends ContentProvider {
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
 
-        // TODO (1) Get access to underlying database (read-only for query)
+        SQLiteDatabase db = new TaskDbHelper(getContext()).getReadableDatabase();
 
-        // TODO (2) Write URI match code and set a variable to return a Cursor
+        // COMPLETED (1) Get access to underlying database (read-only for query)
 
-        // TODO (3) Query for the tasks directory and write a default case
+        // COMPLETED (2) Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
 
-        // TODO (4) Set a notification URI on the Cursor and return that Cursor
+        // COMPLETED (3) Query for the tasks directory and write a default case
+        Cursor cursor;
+        switch (match) {
+            case TASKS:
+                cursor = db.query(TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
 
-        throw new UnsupportedOperationException("Not yet implemented");
+                break;
+            default:
+                throw new UnsupportedOperationException();
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+        // COMPLETED (4) Set a notification URI on the Cursor and return that Cursor
+
     }
 
 
